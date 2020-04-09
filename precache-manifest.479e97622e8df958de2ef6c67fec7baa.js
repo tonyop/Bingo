8ce89fa8fdd71a3f86265352817b93e6a40a2f8d
topipari <topipari@umich.edu>
2020-04-08 23:20:32 -0400

    Updates

---
+++ precache-manifest.479e97622e8df958de2ef6c67fec7baa.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20edc4f8b7cba5be2d43be75d1b4e3d",
+    "url": "/Bingo/index.html"
+  },
+  {
+    "revision": "9804b37f3e79087dc5e4",
+    "url": "/Bingo/static/css/main.f8f2153e.chunk.css"
+  },
+  {
+    "revision": "e6fcb1c4e4586eb30df1",
+    "url": "/Bingo/static/js/2.08af83ab.chunk.js"
+  },
+  {
+    "revision": "5a7d4c221a9788cccdb1086564783a76",
+    "url": "/Bingo/static/js/2.08af83ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9804b37f3e79087dc5e4",
+    "url": "/Bingo/static/js/main.70c76a4f.chunk.js"
+  },
+  {
+    "revision": "22b4b2f282196edfc85a",
+    "url": "/Bingo/static/js/runtime-main.488a9160.js"
+  },
+  {
+    "revision": "a40c304fe0958d0a6c1a615c3540d373",
+    "url": "/Bingo/static/media/B.a40c304f.png"
+  },
+  {
+    "revision": "b68d13d7b35ccc69bee4dbcbfc45cb38",
+    "url": "/Bingo/static/media/G.b68d13d7.png"
+  },
+  {
+    "revision": "a9873fca673e1d554bd69160d067d899",
+    "url": "/Bingo/static/media/I.a9873fca.png"
+  },
+  {
+    "revision": "eba4909f8824a266b9ac90f054a29ea4",
+    "url": "/Bingo/static/media/N.eba4909f.png"
+  },
+  {
+    "revision": "2295cec9a6b3f3ac6e0e3a0379606765",
+    "url": "/Bingo/static/media/O.2295cec9.png"
+  },
+  {
+    "revision": "8c65206a101ccd57f593faca3e4103b7",
+    "url": "/Bingo/static/media/blue_1.8c65206a.png"
+  },
+  {
+    "revision": "9cc1b5fec96c08e54411b6de0d6244f0",
+    "url": "/Bingo/static/media/blue_2.9cc1b5fe.png"
+  },
+  {
+    "revision": "d61bf90f1b195e8cee4292f78e788c4e",
+    "url": "/Bingo/static/media/blue_3.d61bf90f.png"
+  },
+  {
+    "revision": "c86a7610697768ac39c96637890158ed",
+    "url": "/Bingo/static/media/blue_4.c86a7610.png"
+  },
+  {
+    "revision": "3a2a8622b7ea46a17200163430ca49d5",
+    "url": "/Bingo/static/media/blue_5.3a2a8622.png"
+  },
+  {
+    "revision": "59a2570121de55ba4765af8dce54ceba",
+    "url": "/Bingo/static/media/green_1.59a25701.png"
+  },
+  {
+    "revision": "a56f8251c530d733cd77d5ffd2b01d40",
+    "url": "/Bingo/static/media/green_2.a56f8251.png"
+  },
+  {
+    "revision": "f219dbc44e2c1d76c92068393a0f9610",
+    "url": "/Bingo/static/media/green_3.f219dbc4.png"
+  },
+  {
+    "revision": "be70b9660293bb35d464f11676ce743a",
+    "url": "/Bingo/static/media/green_4.be70b966.png"
+  },
+  {
+    "revision": "a196918de0125a247abe1e3b9712ea67",
+    "url": "/Bingo/static/media/green_5.a196918d.png"
+  },
+  {
+    "revision": "7786d61aea6dcf25f9ba987fe796131a",
+    "url": "/Bingo/static/media/pink_1.7786d61a.png"
+  },
+  {
+    "revision": "b40b451bf2b252dbe14ed46d1a7f8aed",
+    "url": "/Bingo/static/media/pink_2.b40b451b.png"
+  },
+  {
+    "revision": "3c9616b2e4a60c3ce3df46ad4bb01d11",
+    "url": "/Bingo/static/media/pink_3.3c9616b2.png"
+  },
+  {
+    "revision": "8a2b39dd1e77fdef29d25ea15890e343",
+    "url": "/Bingo/static/media/pink_4.8a2b39dd.png"
+  },
+  {
+    "revision": "e84137dea106996418599b0b058cb622",
+    "url": "/Bingo/static/media/pink_5.e84137de.png"
+  }
+]);
